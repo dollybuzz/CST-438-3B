0444bb96c4231ca04d0dc4e7e0732ede1da9acb4
Dalia <43527456+dollybuzz@users.noreply.github.com>
2020-03-22 22:23:25 -1000

    uploading complete homework 3 backend

---
+++ src/main/java/CST4383Reservation/ConfigReceiver.java
@@ -0,0 +1,28 @@
+package CST4383Reservation;
+
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+
+@Configuration
+public class ConfigReceiver {
+	@Bean
+	public FanoutExchange fanout() {
+		return new FanoutExchange("city-reservation");
+	}
+	@Bean
+	public Queue queue1() {
+		return new Queue("city-reservation-q1");
+	}
+	@Bean
+	public Binding binding1(FanoutExchange fanout, Queue queue1) {
+		return BindingBuilder.bind(queue1).to(fanout);
+	}
+	@Bean
+	public ReservationEventHandler receiver() {
+		return new ReservationEventHandler();
+	}
+}
